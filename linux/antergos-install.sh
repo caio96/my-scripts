9382cdf1ddaf402861170b07be6f4e6bc054ce29
Caio Salvador <caiosalvador96@gmail.com>
2018-02-26 17:08:16 -0300

    Add antergos install and update hdmi scripts

---
+++ linux/antergos-install.sh
@@ -0,0 +1,78 @@
+# Update
+	sudo pacman -Syu
+
+# Show windowns on grub (dual boot)
+	sudo os-prober
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# Switch lightdm by gmd
+	sudo pacman -S gdm
+	sudo systemctl disable lightdm && sudo systemctl enable gdm
+
+# Add portuguese keyboard and format
+	sudo nano /etc/locale.gen
+	sudo locale-gen
+
+# Dconf-editor and go to org/gnome/nautilus/preferences and set the default-sort-order to "type".
+# Edit energy and privacy options on tweaks
+# Switch dock's click behavior to minimize: tweaks -> extensions -> dash to dock
+# Add hdmi audio scripts
+# Check if /etc/resolve.conf uses opendns
+# Remove 'splash' and 'quiet' from GRUB_CMDLINE_LINUX_DEFAULT in /etc/default/grub to show kernel logs at boot
+
+# Install trizen
+	sudo pacman -S git
+	git clone https://aur.archlinux.org/trizen.git
+	cd trizen
+	makepkg -si
+
+# Use zsh as default shell
+	trizen -S --needed zsh zsh-completions
+	chsh -s $(which zsh)
+	# install oh-my-zsh 
+
+# Install programs
+	trizen -S --needed atom colordiff docker gimp google-chrome gparted gvim 
+	trizen -S --needed intellij-idea-community-edition jdk9-openjdk
+	trizen -S --needed libreoffice-fresh neovim parallel python-pip
+	trizen -S --needed qbittorrent rsync texlive-lang texlive-most
+	trizen -S --needed texstudio tig tmux vlc 
+
+# Gnome extensions support
+	trizen -S --needed chrome-gnome-shell-git
+	# extensions:
+	# alternate tab
+	# caffeine (enable when fullscreen)
+	# dash to dock
+	# lock keys
+	# suspend button
+	# trash
+	# user themes
+
+# AUR
+	trizen -S --needed android-sdk android-sdk-platform-tools android-studio
+	trizen -S --needed google-chrome
+	trizen -S --needed pulseaudio-equalizer-ladspa
+	trizen -S --needed solaar
+	trizen -S --needed spotify
+	trizen -S --needed sublime-text-dev
+	trizen -S --needed visual-studio-code-bin
+
+# Tilix - terminal
+	trizen -S --needed tilix
+	# Add shortcut for tilix and tilix --quake
+    # Edit tilix settings
+
+# YouCompleteMe - cpp completer
+	trizen -S --needed clang
+	# install with vim -> ./install.py --clang-completer --system-libclang
+	
+# Tlp - power manager
+	trizen -S --needed tlp tp_smapi
+	systemctl enable tlp tlp-sleep	
+	systemctl mask systemd-rfkill.service systemd-rfkill.socket
+
+# Powerline
+	sudo pip install powerline-status
+	trizen -S --needed nerd-fonts-complete # DroidSans
+
---
+++ linux/hdmi/hdmi_sound_toggle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#/usr/local/bin/hdmi_sound_toggle.sh
+#/etc/udev/rules.d/99-hdmi_sound.rules
+#/etc/systemd/system/hdmi_sound_toggle.service
+
+USER_NAME=$(who | cut -f 1 -d ' ')
+USER_ID=$(id -u "$USER_NAME")
+HDMI_STATUS=$(cat /sys/class/drm/card0/card0-HDMI-A-1/status)
+
+[[ -z $USER_NAME ]] && exit
+[[ -z $HDMI_STATUS ]] && exit
+
+export PULSE_SERVER="unix:/run/user/"$USER_ID"/pulse/native"
+
+if [[ $HDMI_STATUS == connected ]]
+then
+   sudo -u "$USER_NAME" pactl --server "$PULSE_SERVER" set-card-profile 0 output:hdmi-stereo+input:analog-stereo
+else
+   sudo -u "$USER_NAME" pactl --server "$PULSE_SERVER" set-card-profile 0 output:analog-stereo+input:analog-stereo
+fi
