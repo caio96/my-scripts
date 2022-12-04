28b20aa3b56f917251b5c5f7474bf405676cb64d
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2022-12-04 14:18:17 -0700

    Update ubuntu install

--- linux/ubuntu-22-04-install.sh
+++ linux/ubuntu-22-04-install.sh
@@ -6,43 +6,32 @@ sudo apt update && sudo apt -y upgrade
 sudo apt install -y baobab
 sudo apt install -y build-essential
 sudo apt install -y calibre
-sudo apt install -y chesse
-sudo apt install -y clang
-sudo apt install -y "clang*12"
-sudo apt install -y libclang-12-dev
+sudo apt install -y cheese
+sudo apt install -y "clang*14"
+sudo apt install -y libclang-14-dev
 sudo apt install -y cmake
-sudo apt install -y colordiff
-sudo apt install -y cppcheck
 sudo apt install -y curl
-sudo apt install -y dconf-editor
 sudo apt install -y docker docker-compose
 sudo apt install -y ffmpeg
 sudo apt install -y flameshot
 sudo apt install -y gaupol
 sudo apt install -y gdb
-sudo apt install -y gdebi
 sudo apt install -y gimp
 sudo apt install -y git git-lfs
-sudo apt install -y gnome-tweak-tool
-sudo apt install -y google-perftools
+sudo apt install -y gnome-tweaks
 sudo apt install -y gparted
 sudo apt install -y htop
 sudo apt install -y libreoffice
 sudo apt install -y libtool libtool-bin
 sudo apt install -y linux-tools-common linux-tools-generic
-sudo apt install -y lld-12
-sudo apt install -y llvm
-sudo apt install -y "llvm*12*"
+sudo apt install -y lld-14
+sudo apt install -y "llvm*14*"
 sudo apt install -y locate
 sudo apt install -y meld
 sudo apt install -y neofetch
 sudo apt install -y ninja-build
-sudo apt install -y openconnect
 sudo apt install -y pdftk
-sudo apt install -y psensor
-sudo apt install -y pulseaudio-equalizer
 sudo apt install -y python3 python3-pip python3-dev python3-neovim
-sudo apt install -y qbittorrent
 sudo apt install -y rsync
 sudo apt install -y simple-scan
 sudo apt install -y steam
@@ -53,30 +42,19 @@ sudo apt install -y tlp tlp-rdw
 sudo apt install -y tmux
 sudo apt install -y tree
 sudo apt install -y ubuntu-restricted-extras
-sudo apt install -y uncrustify
 sudo apt install -y vim vim-gtk
 sudo apt install -y vlc
 sudo apt install -y xclip
 sudo apt install -y zsh
 
 # -- Snaps
-sudo snap install intellij-idea-community --classic
-sudo snap install node --classic --channel=14
+sudo snap install node --classic
 sudo snap install slack --classic
+sudo snap install nvim --classic
+sudo snap install code --classic
 sudo snap install spotify
 
-# -- Install Neovim (newer version)
-# https://github.com/neovim/neovim/wiki/Installing-Neovim
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install neovim
-
-# -- Install Open Broadcaster Software Studio
-# https://obsproject.com/wiki/install-instructions#linux
-sudo apt install -y v4l2loopback-dkms
-sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt update
-sudo apt install obs-studio
+# -- Set NVIDIA driver to lastest proprietary version on Software & Updates
 
 # -- Install Papirus Icon Theme
 # https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
@@ -91,17 +69,9 @@ papirus-folders -C yaru --theme Papirus
 # Disable handling of multimedia keys
 # go to chrome://flags/ and disable Hardware Media Key Handling
 
-# -- Install Etcher
-# https://github.com/balena-io/etcher
-# curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | sudo -E bash
-# sudo apt update
-# sudo apt install balena-etcher-electron
-
-# -- Install K2pdfopt
-# https://www.willus.com/k2pdfopt/
-
 # -- Install Mendeley
-# https://www.mendeley.com/download-desktop-new/linux
+# sudo apt install libfuse2
+# https://www.mendeley.com/download-reference-manager/linux
 
 # -- Install Bat
 # https://github.com/sharkdp/bat/releases
@@ -118,22 +88,8 @@ papirus-folders -C yaru --theme Papirus
 # conda update conda
 # conda update --all
 
-# -- Install VS Code
-# https://code.visualstudio.com/docs/setup/linux
-
-# -- Install Youtube-DL
-# https://youtube-dl.org/
-
 # -- Install Zoom
 # https://zoom.us/download
-
-# -- Install Go
-# https://go.dev/dl/
-# move extracted dir to /opt/
-# set gopath to .go
-# go env -w GOPATH=$HOME/.go
-# install pprof after go
-# go install github.com/google/pprof@latest
 
 # -- Install Pip after conda
 # pip install black
@@ -148,14 +104,21 @@ papirus-folders -C yaru --theme Papirus
 # pip install pandas
 # pip install pylint
 # pip install python-language-server
-# pip install youtube_dl
 
 # -- Configure Docker
 # https://docs.docker.com/engine/install/linux-postinstall/
 # sudo groupadd docker
 # sudo usermod -aG docker $USER
 
+# -- Install fzf
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
+
 # -- Configure Prompt:
+
+# - Install starship for bash
+# curl https://starship.rs/install.sh -o install.sh
+# sh install.sh -b $HOME/.local/bin
 
 # - Install oh-my-zsh
 # https://ohmyz.sh/#install
@@ -174,14 +137,6 @@ papirus-folders -C yaru --theme Papirus
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # plugins=([plugins...] zsh-autosuggestions) in .zshrc
 
-# - Install bash-it
-# git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-# ~/.bash_it/install.sh
-
-# - Install starship for bash
-# curl https://starship.rs/install.sh -o install.sh
-# sh install.sh -b $HOME/.local/bin
-
 # - Install vim-plug for vim and neovim
 # https://github.com/junegunn/vim-plug
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -197,8 +152,13 @@ papirus-folders -C yaru --theme Papirus
 # chmod 644 ~/.ssh/config
 
 # -- Setup Tilix
-# Set theme to Gruvbox Dark https://github.com/MichaelThessel/tilix-gruvbox.git
+
 # Set font to FiraCode Nerd Font Regular 11.5
+# Set theme to Gruvbox Dark Hard 
+# git clone https://github.com/MichaelThessel/tilix-gruvbox.git
+# cd tilix-gruvbox
+# mkdir -p ~/.config/tilix/schemes
+# cp gruvbox-* ~/.config/tilix.schemes/
 
 # -- Retain less olders version in snap
 sudo snap set system refresh.retain=2
@@ -214,3 +174,34 @@ sudo tlp start
 
 # -- Clean up
 sudo apt -y autoremove && sudo apt -y clean
+
+# -- Other software
+
+# -- Install Open Broadcaster Software Studio
+# https://obsproject.com/wiki/install-instructions#linux
+# sudo apt install -y v4l2loopback-dkms
+# sudo add-apt-repository ppa:obsproject/obs-studio
+# sudo apt update
+# sudo apt install obs-studio
+
+# -- Install Etcher
+# https://github.com/balena-io/etcher
+# curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | sudo -E bash
+# sudo apt update
+# sudo apt install balena-etcher-electron
+
+# -- Install K2pdfopt
+# https://www.willus.com/k2pdfopt/
+
+# -- Install Go
+# https://go.dev/dl/
+# move extracted dir to /opt/
+# set gopath to .go
+# go env -w GOPATH=$HOME/.go
+# install pprof after go
+# go install github.com/google/pprof@latest
+
+# -- Install Youtube-DL
+# https://youtube-dl.org/
+# pip install youtube_dl
+
